febd32bfc538e041843808fba474d92d4f1b8bad
Felipe <flpc.dev@gmail.com>
2023-04-13 19:07:38 -0300

    feat: add routes to home and movie details pages

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,21 @@
+import { MovieDetails } from "@screens/MovieDetails";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Home } from "@screens/Home";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen 
+        name="home"
+        component={Home}
+      />
+
+    <Screen 
+        name="movieDetails"
+        component={MovieDetails}
+      />
+    </Navigator>
+  )
+}
