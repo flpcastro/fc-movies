62afb7f50d074a48d0aaf7d558a908ef06836525
Felipe <flpc.dev@gmail.com>
2023-04-13 18:56:10 -0300

    feat: add babel plugin module resolver configs

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@theme': './src/theme',
+            '@screens': './src/screens',
+            '@routes': './src/routes',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@store': './src/store'
+          }
+        }
+      ],
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env"
+      }],
+    ]
   };
 };
