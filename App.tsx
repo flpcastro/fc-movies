3bb3319b548e34df6a3fdbfa81b2da5ddc80763a
Felipe <flpc.dev@gmail.com>
2023-04-13 18:59:38 -0300

    feat: principal file implemented with redux provider, theme provider and routes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'react-native';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from '@theme/index';
+
+import { Routes } from '@routes/index';
+import { Loading } from '@components/Loading';
+import { Provider } from 'react-redux';
+import { Store } from './src/store/storeConfig';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <View>
-      <Text>Hello Word</Text>
-    </View>
-  );
+    <Provider store={Store}>
+      <ThemeProvider theme={theme}>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        { fontsLoaded ? <Routes /> : <Loading />}
+      </ThemeProvider>
+    </Provider>
+  )
 }
 
